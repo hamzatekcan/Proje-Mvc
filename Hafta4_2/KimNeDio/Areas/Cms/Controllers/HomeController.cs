b2ee4614e9f05e05d8dd8629e12cd04d536e98b9
Hamza Tekcan <hamzatekcan@gmail.com>
2018-06-20 08:51:59 +0300

    v1

---
+++ Hafta4_2/KimNeDio/Areas/Cms/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KimNeDio.Areas.Cms.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Cms/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult NotificationPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AuthorMessagePartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AuthorListPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult LatestPostsPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult LatestCommentsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Hafta4_2/KimNeDio/Areas/Cms/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KimNeDio.Areas.Cms.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Cms/Post
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
